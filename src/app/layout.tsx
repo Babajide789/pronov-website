3623592ceba5668f1647117f572abd46099a141b
Babajide789 <yahayababajide@gmail.com>
2025-11-08 01:28:48 -0500

    tweak for vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import SidebarWrapper from "@/components/SidebarWrapper";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 
 
